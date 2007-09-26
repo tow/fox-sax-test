57461b82bf1cd19eaf444f1b2debba9779bfae4b
Toby White <tow21@cam.ac.uk>
2007-09-26 15:34:30 +0100

    Improved test harness

--- runtests.sh
+++ runtests.sh
@@ -2,18 +2,26 @@
 
 THISPWD=$(pwd)
 XSL=$(tempfile)
+TESTS=$(tempfile)
 TMPFILE=$(tempfile)
-SAX_WELL_FORMED=$(pwd)/sax_well_formed.exe
-SAX_VALID=$(pwd)/sax_valid.exe
+TEST=$THISPWD/test.sh
+XMLCONF=$THISPWD/xmlconf.xml
+SAX_WELL_FORMED=$THISPWD/sax_well_formed
+SAX_VALID=$THISPWD/sax_valid
 
 PASSED=$THISPWD/PASSED.out
 FAILED=$THISPWD/FAILED.out
 LEAKED=$THISPWD/LEAKED.out
+CRASHED=$THISPWD/CRASHED.out
 XFAIL=$THISPWD/XFAIL
 XPASS=$THISPWD/XPASS.out
 
-if [ $# -eq 1 ]; then
-  testFiles=$1
+export TEST TMPFILE SAX_WELL_FORMED SAX_VALID
+export PASSED FAILED LEAKED CRASHED XFAIL XPASS
+
+if [ $# -eq 2 ]; then
+  testDir=$1
+  testFile=$2
   all=no
 else
   testFiles=
@@ -27,7 +35,6 @@ fi
 cat <<EOF > $XSL
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
   <xsl:output method="text"/>
-  <xsl:param name="type"/>
   <xsl:template match="text()"/>
   <xsl:template name="base">
     <xsl:choose>
@@ -45,132 +52,31 @@ cat <<EOF > $XSL
     </xsl:choose>
   </xsl:template>  
   <xsl:template match="TEST">
-    <xsl:if test="@TYPE=\$type">
+    <xsl:variable name="base">
       <xsl:call-template name="base"/>
-      <xsl:value-of select="@URI"/>
-      <xsl:text>
-</xsl:text>
+    </xsl:variable>
+    <xsl:variable name="ns" select="substring('yesno', 3*number(@NAMESPACE='no')+1, 3)"/>
+    <xsl:if test="@ENTITIES='none'">
+      <xsl:value-of select="concat('(cd ', \$base,';')"/>
+      <xsl:value-of select="concat('\$TEST ', @TYPE,' ', \$ns)"/>
+      <xsl:value-of select="concat(' ',\$base,' ',@URI,')&#10;')"/>
     </xsl:if>
   </xsl:template>
 </xsl:stylesheet>
 EOF
 
-for i in $(xsltproc --param type "'not-wf'" $XSL xmlconf.xml); do
-  if [ ! -z $testFiles ] && [ ! $testFiles = $i ]; then
-    continue
-  fi
-  echo Checking that we detect not-well-formedness: $i
-  DIR=$(echo $i | sed -e 's/\(.*\)\/.*/\1/')
-  FILE=$(echo $i | sed -e 's/.*\/\(.*\)/\1/')
-  cd $DIR
-  if [ -f test.xml ]; then
-    echo "Problem! Found a pre-existing file called test.xml"
-    echo "Stopping now before I overwrite it."
-    exit 1
-  fi
-  ln -s $FILE test.xml
-  ($SAX_WELL_FORMED > test.out 2>&1)
-  if grep Error test.out > /dev/null
-  then
-    if grep $i $XFAIL > /dev/null; then
-      echo $i >> $XPASS
-    fi
-    echo $i >> $PASSED
-    grep -v $i $FAILED > $TMPFILE; mv $TMPFILE $FAILED
-  else
-    if ! grep $i $XFAIL > /dev/null; then
-      echo Couldnt find this in XFAIL
-      echo $i >> $FAILED
-    else
-      grep -v $i $FAILED > $TMPFILE; mv $TMPFILE $FAILED
-    fi
-    grep -v $i $PASSED > $TMPFILE; mv $TMPFILE $PASSED
-  fi
-  if grep Remaining test.out > /dev/null; then
-    echo $i >> LEAKED.out
-  fi
-  rm test.xml
-  rm test.out
-  cd $THISPWD
-done
+xsltproc $XSL xmlconf.xml > $TESTS
+if [ ! -z $testFile ]; then
+  eval $(grep $testDir $TESTS | grep $testFile)
+else
+  . $TESTS
+fi
 
- # for i in $(xsltproc --param type "'invalid'" $XSL xmlconf.xml); do
-#   if [ ! -z $testFiles ] && [ ! $testFiles = $i ]; then
-#     continue
-#   fi
-#   echo Checking that we detect invalidity: $i
-#   THISPWD=$(pwd)
-#   DIR=$(echo $i | sed -e 's/\(.*\)\/.*/\1/')
-#   FILE=$(echo $i | sed -e 's/.*\/\(.*\)/\1/')
-#   cd $DIR
-#   if [ -f test.xml ]; then
-#     echo "Problem! Found a pre-existing file called test.xml"
-#     echo "Stopping now before I overwrite it."
-#     exit 1
-#   fi
-#   ln -s $FILE test.xml
-# # Check passes well-formedness if we don't check validity
-#   if $SAX_WELL_FORMED | grep Error > /dev/null
-#   then
-#     echo $i >> $PASSED
-#   else 
-#     echo $i >> $FAILED
-#   fi
-# # Check fails validity if we do.
-#   if $SAX_VALID | grep Error > /dev/null
-#   then
-#     echo $i >> $FAILED
-#   else 
-#     echo $i >> $PASSED
-#   fi
-#   rm test.xml
-#   cd $THISPWD
-# done
-
-for i in $(xsltproc --param type "'valid'" $XSL xmlconf.xml); do
-  if [ ! -z $testFiles ] && [ ! $testFiles = $i ]; then
-    continue
-  fi
-  echo Checking that we detect validity: $i
-  THISPWD=$(pwd)
-  DIR=$(echo $i | sed -e 's/\(.*\)\/.*/\1/')
-  FILE=$(echo $i | sed -e 's/.*\/\(.*\)/\1/')
-  cd $DIR
-  if [ -f test.xml ]; then
-    echo "Problem! Found a pre-existing file called test.xml"
-    echo "Stopping now before I overwrite it."
-    exit 1
-  fi
-  ln -s $FILE test.xml
-  ($SAX_VALID > test.out 2>&1)
-  if grep Error test.out > /dev/null
-  then
-    if ! grep $i $XFAIL > /dev/null; then
-      echo Couldnt find this in XFAIL
-      echo $i >> $FAILED
-    else
-      grep -v $i $FAILED > $TMPFILE; mv $TMPFILE $FAILED
-    fi
-    grep -v $i $PASSED > $TMPFILE; mv $TMPFILE $PASSED
-  else 
-    if grep $i $XFAIL > /dev/null; then
-      echo $i >> $XPASS
-    fi
-    echo $i >> $PASSED
-    grep -v $i $FAILED > $TMPFILE; mv $TMPFILE $FAILED
-  fi
-  if grep Remaining test.out > /dev/null; then
-    echo $i >> LEAKED.out
-  fi
-  rm test.xml
-  cd $THISPWD
-done
-
-if [ ! -z $testFiles ]; then
+if [ ! -z $testFile ]; then
   sort $PASSED | uniq > $TMPFILE; mv $TMPFILE $PASSED
   sort $FAILED | uniq > $TMPFILE; mv $TMPFILE $FAILED
   sort $LEAKED | uniq > $TMPFILE; mv $TMPFILE $LEAKED
   sort $XPASS | uniq > $TMPFILE; mv $TMPFILE $XPASS
 fi
 
-rm -f $XSL $TMPFILE
+rm -f $XSL $TMPFILE $TESTS
---
+++ test.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+if [ $# -ne 4 ]; then
+  echo Usage: test.sh \$TYPE \$NAMESPACE \$BASE \$URI
+  exit 1
+fi
+
+TYPE=$1
+NAMESPACE=$2
+BASE=$3
+URI=$4
+
+ln -s $URI test.xml
+fail=no
+leaked=noo
+echo -n "Checking $BASE$URI for $TYPE "
+
+case $TYPE in
+  not-wf)
+    ($SAX_WELL_FORMED.ns.$NAMESPACE.exe > test.out 2>&1)
+    if ! grep Error test.out > /dev/null; then
+      fail=yes
+    fi
+    break
+    ;;
+
+  valid)
+    ($SAX_VALID.ns.$NAMESPACE.exe > test.out 2>&1)
+    if grep Error test.out > /dev/null; then
+      fail=yes
+    fi
+    break
+    ;;
+
+  invalid)
+    ($SAX_WELL_FORMED.ns.$NAMESPACE.exe > test.out 2>&1)
+    if grep Error test.out > /dev/null; then
+      fail=yes
+    fi
+    ($SAX_VALID.ns.$NAMESPACE.exe > test.out 2>&1)
+    if ! grep Error test.out > /dev/null; then
+      fail=yes
+    fi
+    break
+    ;;
+
+  error)
+    ($SAX_VALID.ns.$NAMESPACE.exe > test.out 2>&1)
+    if ! grep Error test.out > /dev/null; then
+      fail=yes
+    fi
+    break
+    ;;
+esac
+
+if ! grep Finished test.out > /dev/null; then
+  echo $BASE $URI >> $CRASHED
+  exit 0
+else
+  grep -v "$BASE $URI" $CRASHED > $TMPFILE; mv $TMPFILE $CRASHED
+fi
+
+if grep Remaining test.out > /dev/null; then
+  leaked=yes
+fi
+rm -f test.xml test.out
+
+if [ $fail = yes ]; then
+  echo failed
+  if grep "$BASE$URI" $XFAIL > /dev/null; then
+    grep -v "$BASE $URI" $FAILED > $TMPFILE; mv $TMPFILE $FAILED
+    echo $BASE$URI $TYPE >> $XFAIL.out
+  else
+    echo $BASE $URI $TYPE >> $FAILED
+  fi
+  grep -v "$BASE $URI" $PASSED > $TMPFILE; mv $TMPFILE $PASSED
+else
+  echo passed
+  if grep "$BASE$URI" $XFAIL > /dev/null; then
+    echo $BASE $URI $TYPE >> $XPASS
+  else
+  grep -v "$BASE $URI" $FAILED > $TMPFILE; mv $TMPFILE $FAILED
+  echo $BASE $URI $TYPE >> $PASSED
+  fi
+fi
