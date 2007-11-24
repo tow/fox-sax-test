815e3c0f500eea1a1ff955ce4bfeb2e2c10347db
Toby White <tow21@cam.ac.uk>
2007-11-24 19:01:01 +0000

    Ensure tests are runwith latest copy of FoX

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+FOXHOME=/Users/tow/devel/FoX
+
+rm *.exe
+(cd $FOXHOME/sax/test;
+make clean sax_well_formed.ns.no.exe sax_well_formed.ns.yes.exe sax_valid.ns.no.exe sax_valid.ns.yes.exe)
+cp $FOXHOME/sax/test/sax_well_formed.*.exe .
+cp $FOXHOME/sax/test/sax_valid.*.exe .
+
 THISPWD=$(pwd)
 XSL=$(tempfile)
 TESTS=$(tempfile)
